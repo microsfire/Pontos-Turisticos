723ed14a9346aeb25b173cbea586ae531f0d17f5
microsfire <microsfire@msn.com>
2024-06-04 20:23:14 -0300

    Habilitando o sistema de tokens

--- pontosTuristicos/urls.py
+++ pontosTuristicos/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views
 
 from apps.core.api.viewsets import PontoTuristicoViewSet
 from apps.atracoes.api.viewsets import AtracaoViewSet
@@ -39,5 +40,6 @@ router.register(r'enderecos', EnderecoViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
