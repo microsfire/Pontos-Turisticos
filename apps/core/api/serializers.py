b7d79d6a208aac371282cf9f2822aa0db29d551e
microsfire <microsfire@msn.com>
2024-06-06 14:02:40 -0300

    exibindo os campos serializados dos relacionamentos

--- apps/core/api/serializers.py
+++ apps/core/api/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 from apps.core.models import PontoTuristico
+from apps.atracoes.api.serializers import AtracaoSerializers
+from apps.enderecos.api.serializers import EnderecoSerializers
+from apps.comentarios.api.serializers import ComentarioSerializers
+from apps.avaliacoes.api.serializers import AvaliacaoSerializers
 
 
 class PontoTuristicoSerializers(ModelSerializer):
+    atracoes = AtracaoSerializers(many=True)
+    comentarios = ComentarioSerializers(many=True)
+    avaliacoes = AvaliacaoSerializers(many=True)
+    endereco = EnderecoSerializers()
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto']
+        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes', 'comentarios',
+                  'avaliacoes', 'endereco', 'foto']
 
 
 
